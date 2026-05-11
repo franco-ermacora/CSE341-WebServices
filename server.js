52ad9ef02e0602c14469c2864e0ec91ceeba00ec
Franco Ermacora <ermacora.fran@gmail.com>
2026-05-11 00:37:37 -0300

    W01 Project: Contacts Part 1 V0.0

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
 const port = process.env.PORT || 8080;
+const app = express();
 
-app.use(cors());
-// Aquí le decimos que use la carpeta routes
-app.use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+// Esta es la parte clave: intenta conectar antes de escuchar
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
