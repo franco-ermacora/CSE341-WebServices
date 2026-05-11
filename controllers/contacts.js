acd7a8bdcbe4c30f8ccee4f4e01091b59ffbdbd4
Franco Ermacora <ermacora.fran@gmail.com>
2026-05-11 00:48:01 -0300

    Fix single contact route

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,11 +10,12 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
+  const userId = new ObjectId(req.params.id); 
   const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+  
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
+    res.status(200).json(lists[0]); 
   });
 };
 
